ace69a39b3a8465d765e9194920fedac3f9060c9
Irving Zhao <irvinezhao@gmail.com>
2018-07-09 16:55:26 +0800

    Data & Methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el:'#vue-app',
   data: {
-    name: 'Irving'
+    name: 'Irving',
+    job: 'dev'
+  },
+  methods:{
+    greet: function(time){
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
 });

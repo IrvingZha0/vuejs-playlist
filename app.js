c492a2e9dcce8056b4e5b663c12568d3c2cca9e7
Irving Zhao <irvinezhao@gmail.com>
2018-07-09 18:07:58 +0800

    Computed Properties

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 new Vue({
   el:'#vue-app',
   data: {
-    name: '',
-    age:''
+    age: 25,
+    a: 0,
+    b: 0
   },
   methods:{
-  
+    // addToA: function(){
+    //   console.log('a is run')
+    //   return this.a + this.age
+    // },
+    // addToB: function(){
+    //   console.log('b is run')
+    //
+    //   return this.b + this.age
+    // }
+  },
+  computed:{
+    addToA: function(){
+      console.log('a is run')
+      return this.a + this.age
+    },
+    addToB: function(){
+      console.log('b is run')
+      return this.b + this.age
     }
   }
 });
